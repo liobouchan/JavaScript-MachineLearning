f85cea236e18c261ce3985d71db075f840768721
Leonardo <liogothicvampire@gmail.com>
2018-10-30 11:33:39 -0600

    Regulations

---
+++ SentencesDetection/BrainJSRegulations/js/scripts.js
@@ -0,0 +1,44 @@
+let trainedNet;
+
+function encode(arg) {
+    return arg.split('').map(x => (x.charCodeAt(0) / 256));
+}
+
+function processTrainingData(data) {
+    return data.map(d => {
+        return {
+            input: encode(d.input),
+            output: d.output
+        }
+    })
+}
+
+function train(data) {
+    let net = new brain.NeuralNetwork();
+    net.train(processTrainingData(data));
+    trainedNet = net.toFunction();
+};
+
+function execute(input) {
+    let results = trainedNet(encode(input));
+    console.log(results)
+    let output;
+    let certainty;
+    if (results.obligacion > results.mandato) {
+        output = 'Obligacion'
+        certainty = Math.floor(results.obligacion * 100)
+    } else { 
+        output = 'Mandato'
+        certainty = Math.floor(results.mandato * 100)
+    }
+
+    return "I'm " + certainty + "% sure that tweet was written by " + output;
+}
+
+train(trainingData);
+console.log(execute("Tratándose de una Metodología Interna con enfoque Básico, obteniendo la Probabilidad de Incumplimiento de sus posiciones sujetas a riesgo de crédito. Para el resto de los componentes del riesgo, las Instituciones deberán ajustarse a lo establecido en el referido Apartado C de esta sección. "));
+console.log(execute("Las instituciones estan obligadas a presentar los hechos al jurado"));
+console.log(execute("Se Obliga a cumplir con lo estipulado"));
+console.log(execute("Las partes se obligarán a cumplir con sus contratos"));
+console.log(execute("Están obligados por la ley a intervenir"));
+
---
+++ SentencesDetection/BrainJSRegulations/js/training-data.js
@@ -0,0 +1,51 @@
+const trainingData = [
+    {
+        input: "Deberás cumplir con lo siguiente:",
+        output: { mandato: 1 }
+    },{
+        input: "las mencionadas entidades financieras deberán sujetarse en el desarrollo de sus operaciones, lo que también habrá de facilitar la consulta, cumplimiento y observancia  de las disposiciones que les resultan ser aplicables, ha resuelto expedir las siguientes", 
+        output: { mandato: 1 }
+    },{
+        input: "Las Instituciones deberán solicitar autorización de la Comisión para emplear una de las metodologías señaladas.", 
+        output: { mandato: 1 }
+    },{
+        input: "por lo que deberán acreditar el cumplimiento de lo establecido en la presente sección, en la Sección Cuarta del Capítulo V del Título Segundo", 
+        output: { mandato: 1 }
+    },{
+        input: "Presentar a la Comisión un plan de implementación de la Metodología Interna, el cual deberá incluir los aspectos siguientes", 
+        output: { mandato: 1 }
+    },{
+        input: "Dicho plan deberá señalar la extensión y tiempos en que las Instituciones aplicarán las metodologías señaladas para las diferentes clases de activos y unidades de negocio, debiendo ser exhaustivo y viable.", 
+        output: { mandato: 1 }
+    },{
+        input: "El plan de implementación de la Metodología Interna deberá incluir cuando menos los aspectos siguientes:", 
+        output: { mandato: 1 }
+    },{
+        input: "La autoevaluación será responsabilidad del Director General quien, para su elaboración, deberá apoyarse en el área de Auditoría Interna, la cual será responsable de vigilar que los procesos de validación hayan sido aplicados correctamente y que cumplen los propósitos para los cuales fueron diseñados.", 
+        output: { mandato: 1 }
+    },{
+        input: "Las Instituciones a fin de utilizar Metodologías Internas para calcular sus requerimientos de capital por riesgo de crédito, deberán observar las condiciones generales siguientes:", 
+        output: { mandato: 1 }
+    },{
+        input: "Deberán emplear sus propias estimaciones de los componentes del riesgo a fin de determinar el requerimiento de capital correspondiente a sus posiciones sujetas a riesgo de crédito, conforme al Apartado C de la presente sección.", 
+        output: { mandato: 1 }
+    },{
+        input: "obligación incondicional a cargo de la institución de banca múltiple emisora de que el monto del Capital Fundamental", 
+        output: { obligacion: 1 }
+    },{
+        input: "Dichos organismos públicos se obligan al pago de una tarifa para cubrir la inversión financiada con deuda, y", 
+        output: { obligacion: 1 }
+    },{
+        input: "Las Instituciones estarán obligadas a mantener capital conforme a lo establecido en el presente apartado para la totalidad de sus posiciones vinculadas a Esquemas de Bursatilización", 
+        output: { obligacion: 1 }
+    },{
+        input: "estará obligada a mantener capital para todos los activos subyacentes de la citada estructura que se estén respaldando", 
+        output: { obligacion: 1 }
+    },{
+        input: "La Institución Originadora de Esquemas de Bursatilización estará obligada a constituir capital tanto para los saldos no ejercidos que le correspondan", 
+        output: { obligacion: 1 }
+    },{
+        input: "Se Obliga a cumplir con lo estipulado", 
+        output: { obligacion: 1 }
+    }
+]
